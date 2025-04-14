038be516861b88cac2110dcbb92a2d61006ad661
alinalitvinka <alinalitvinko7@gmail.com>
2025-04-14 14:58:55 +0300

    feat: solve 10th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -240,8 +240,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let cache = startFrom;
+  const getId = function getId() {
+    const result = cache;
+    cache += 1;
+    return result;
+  };
+  return getId;
 }
 
 module.exports = {
