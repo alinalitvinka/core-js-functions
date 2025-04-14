9dff80f0ea65e381d8f689e5d3d9be0482c47cb7
alinalitvinka <alinalitvinko7@gmail.com>
2025-04-14 14:35:45 +0300

    feat: solve 9th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -216,8 +216,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const result = function res(...args2) {
+    return fn(...args1, ...args2);
+  };
+  return result;
 }
 
 /**
